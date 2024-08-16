019300c32cbf64e10b451a5e594d498b56cfdd75
CristianMorales1999 <cm9225064@gmail.com>
2024-08-16 07:15:24 -0500

    Variables de Entorno-Migraciones

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::view('/','home')->name('home');
-
-// Route::get('servicios','App\Http\Controllers\ServiciosController@index')->name('servicios.index');
 Route::resource('servicios','App\Http\Controllers\ServiciosController');
-// Route::get('proyectos','App\Http\Controllers\ProyectosController@index')->name('proyectos.index');
 Route::resource('proyectos','App\Http\Controllers\ProyectosController');
-// Route::get('clientes','App\Http\Controllers\ClientesController@index')->name('clientes.index');
 Route::resource('clientes','App\Http\Controllers\ClientesController');
-
 Route::view('blog','blog')->name('blog');
 Route::view('contacto','contacto')->name('contacto');
